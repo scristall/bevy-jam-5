fae6f080076bda538adcef8911c9217fa99aedb0
Thia Wyrod <thia@wyrod.ca>
2024-07-25 22:09:43 -0700

    CI/clippy: Really shut it up with camera webgl2 workaround.

--- src/camera.rs
+++ src/camera.rs
@@ -1,18 +1,21 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
 
+const PIXELATE_BLOCK_SIZE: f32 = 2.0;
+
 #[derive(Component)]
 pub struct MainCamera;
 
+#[allow(clippy::field_reassign_with_default)]
 fn setup(mut commands: Commands) {
     let mut camera = Camera2dBundle::default();
     camera.projection.scaling_mode = ScalingMode::FixedVertical(1080.0);
+    let mut settings = crate::pixelate::PixelateSettings::default();
+    settings.block_size = PIXELATE_BLOCK_SIZE;
+
     commands.spawn((
         camera,
         MainCamera,
-        crate::pixelate::PixelateSettings {
-            block_size: 2.0,
-            ..default()
-        },
+        settings,
     ));
 }
 
