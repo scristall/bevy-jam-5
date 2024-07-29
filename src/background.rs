a00f9a0cc34fe6028dce6c1d68dbb344e2096ad1
Sam Cristall <sam.cristall@gmail.com>
2024-07-29 00:20:59 -0600

    Remove dead text

--- src/background.rs
+++ src/background.rs
@@ -8,23 +8,6 @@ use std::fmt::Write;
 struct FreqText;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section(
-                "600 kHz",
-                TextStyle {
-                    color: Color::BLACK,
-                    font: asset_server.load("fonts/Belleza-Regular.ttf"),
-                    font_size: 80.0,
-                },
-            )
-            .with_justify(JustifyText::Center),
-            transform: Transform::from_rotation(Quat::from_rotation_z(-0.04))
-                .with_translation(Vec3::new(590.0, -185.0, 1.0)),
-            ..default()
-        },
-        FreqText,
-    ));
 }
 
 fn update(
